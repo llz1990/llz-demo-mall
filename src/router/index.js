494ec2fc935d9991b3c63dbde1b9c644ee011752
linzhen <linzhen@shubida.com>
2020-03-22 15:11:50 +0800

    项目分类

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+Vue.use(Router)    // Vue实例引用路由模块
+const ROUTES = [
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+]
+
+const ROUTER = new Router({
+  routes: ROUTES
+});
+
+export default ROUTER;
