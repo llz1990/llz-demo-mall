dc5f1992ba32195b8ad26a0ed2d154533c6d7f92
linzhen <linzhen@shubida.com>
2020-04-06 13:16:07 +0800

    20200406——加入 better-scroll 滚动效果

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -1,34 +1,31 @@
 <template>
   <div ref="wrapper" class="wrapper">
-    <div>
+    <div class="content">
       <slot></slot>
     </div>
   </div>
 </template>
 
 <script>
-import BScroll from 'better-scroll';
+import BScroll from "better-scroll";
 export default {
-    name: 'Scroll',
-    data() {
-        return {
-            scroll: null
-        }
-    },
-    mounted() {
-        setTimeout(this._initScroll, 1000)
-    },
-    methods: {
-        // 初始化、创建BetterScroll
-        _initScroll() {
-            this.scroll = new BScroll(this.$refs['wapper'], {
-            })
-        },
-    }
-
-}
+  name: "Scroll",
+  data() {
+    return {
+      scroll: null
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.scroll = new BScroll(this.$refs.wrapper, {});
+    });
+  },
+  methods: {}
+};
 </script>
 
 <style scoped>
-
+.wrapper{
+  height: 600px;
+}
 </style>
